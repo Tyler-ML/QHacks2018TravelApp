e0031c8b612e9af4c1ef14fe9806ce899f0d53fa
James <jameslezadabonifacio@gmail.com>
2018-02-03 12:25:04 -0500

    Added button listener

--- app/src/main/java/com/example/james/travelhack/MainActivity.java
+++ app/src/main/java/com/example/james/travelhack/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.james.travelhack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnNext = (Button) findViewById(R.id.btnNext);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Is called when button is clicked, put function here
+
+
+            }
+        });
     }
 }
