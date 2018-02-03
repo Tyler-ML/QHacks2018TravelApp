f3efb8b12603da5c875b8fd4f85e3e8b455c960e
Marcus Chan <15msc@queensu.ca>
2018-02-03 15:02:50 -0500

    Got the button to work

--- app/src/main/java/com/example/james/travelhack/MainActivity.java
+++ app/src/main/java/com/example/james/travelhack/MainActivity.java
@@ -1,27 +1,29 @@
 package com.example.james.travelhack;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+//import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnNext = (Button) findViewById(R.id.btnNext);
-
-        btnNext.setOnClickListener(new View.OnClickListener() {
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                //Is called when button is clicked, put function here
-
-
+            public void onClick(View v) {
+                openSwipeActivity();
             }
         });
+    }
+    public void openSwipeActivity(){
+    Intent intent = new Intent(this,SwipeActivity.class);
+    startActivity(intent);
     }
 }
